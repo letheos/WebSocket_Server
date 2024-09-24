bc48077559e7ab102789696e0c53e1ea30493351
Aymeric Morin <aymericmorin59@gmail.com>
2024-09-24 14:10:38 +0200

    Permier commit du serveur Spring

---
+++ src/main/java/fr/uphf/Questease/QuesteaseApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.Questease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesteaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuesteaseApplication.class, args);
+	}
+
+}

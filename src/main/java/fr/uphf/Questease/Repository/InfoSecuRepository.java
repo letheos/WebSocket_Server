7477ac42a0a63bec4f1daf9016f25492437a78a9
Rafale2000 <loick1morneau@icloud.com>
2024-10-18 08:23:37 +0200

    repo fini

--- src/main/java/fr/uphf/Questease/Repository/InfoSecuRepository.java
+++ src/main/java/fr/uphf/Questease/Repository/InfoSecuRepository.java
@@ -1,5 +1,6 @@
 package fr.uphf.Questease.Repository;
 
+import fr.uphf.Questease.Model.InfoSecu;
 import fr.uphf.Questease.Model.Utilisateur;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -7,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InfoSecuRepository extends CrudRepository<Utilisateur, Long> {
+public interface InfoSecuRepository extends CrudRepository<InfoSecu, Long> {
 
 }

d4bd4063648638dab80f051843af55c97dd30485
Aymeric Morin <aymericmorin59@gmail.com>
2024-10-22 12:17:27 +0200

    Fin de la javadoc

--- src/main/java/fr/uphf/Questease/Controller/MotCryptexController.java
+++ src/main/java/fr/uphf/Questease/Controller/MotCryptexController.java
@@ -12,39 +12,70 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.random.RandomGenerator;
 
+/**
+ * Controller de la classe MotCryptex
+ */
 @RestController
 @RequestMapping
 public class MotCryptexController {
 
+    /**
+     * Service du repositoire de MotCryptex
+     */
     private final MotCryptexServiceImpl repo;
 
+    /**
+     * Contructeur de la classe MotCryptexController
+     * @param repo le service du repositoire de MotCryptex
+     */
     public MotCryptexController(MotCryptexServiceImpl repo) {
         this.repo = repo;
     }
 
-
+    /**
+     * Méthode Get permettant de récupérer un mot via son id
+     * @param idMotCryptex L'id du mot récupéré
+     * @return Le mot récupéré
+     */
     @GetMapping("/idMotCryptex")
     public Optional<MotCryptex> getMotById(@PathVariable Long idMotCryptex) {
         return repo.FetchMotCryptex(idMotCryptex);
     }
 
+    /**
+     * Méthode get permettant de récupérer in mot aléatoire de la base de donnée
+     * @return Un mot aléatoire de la base de donnée
+     */
     @GetMapping("MotCryptex")
     public MotCryptex getRandomWord(){
         List<MotCryptex> liste = repo.FetchMotCryptexList();
         return liste.get(0 + (int)(Math.random() * ((liste.size() - 0) + 1)));
     }
 
+    /**
+     * Méthode Post permettant d'ajouter un mot à la base de donnée
+     * @param M Le mot qui sera ajouté à la base de donnée
+     */
     @PostMapping("/{idMotCryptex}")
     public void PostIndice(@PathVariable MotCryptex M) {
         repo.SaveMotCryptex(M);
     }
 
+    /**
+     * Méthode Update permettant de mettre à jour un mot de la base de donnée
+     * @param IdMot L'id du mot qui sera mis à jour
+     * @param M Le mot qui sera mis à jour
+     */
     @PatchMapping("/{idMotCryptex}")
     public void UpdateMot(Long IdMot, @PathVariable MotCryptex M) {
         repo.deleteMotCryptex(IdMot);
         repo.SaveMotCryptex(M);
     }
 
+    /**
+     * Méthode Delete permettant de supprimer un mot de la base de donnée
+     * @param M Le mot qui sera supprimé de la base de donnée
+     */
     @DeleteMapping("/{idMoCryptex}")
     public void DeleteMot(@PathVariable MotCryptex M) {
         repo.deleteMotCryptex(M.getId());

106defab5ef68fd58b2afed7b65fe4ccb57694e6
Rafale2000 <loick1morneau@icloud.com>
2024-11-06 16:18:42 +0100

    ajout de la classe java

---
+++ src/main/java/fr/uphf/Questease/Model/Lobby.java
@@ -0,0 +1,36 @@
+package fr.uphf.Questease.Model;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Lobby")
+public class Lobby {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idLobby")
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "lobby_id") // Foreign key column in the JoueurTmp table
+    private List<JoueurTmp> joueurs = new ArrayList<>();
+
+    // Constructors, getters, setters, and other methods
+
+    //pas de constructeur par défault, obligé d'avoir
+    public Lobby(JoueurTmp host){
+        joueurs.add(host);
+    }
+
+    public List<JoueurTmp> getJoueurs() {
+        return joueurs;
+    }
+
+    public void setJoueurs(List<JoueurTmp> joueurs) {
+        if (joueurs.size() > 2) {
+            throw new IllegalArgumentException("A lobby can have a maximum of 2 players.");
+        }
+        this.joueurs = joueurs;
+    }
+}

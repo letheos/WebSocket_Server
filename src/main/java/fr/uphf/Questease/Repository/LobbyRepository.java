adb7ebe421d3f1bc2aae53d15ff711c0ce646680
theo <102145780+letheos@users.noreply.github.com>
2024-11-29 12:17:00 +0100

    Delete src directory

--- src/main/java/fr/uphf/Questease/Repository/LobbyRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.uphf.Questease.Repository;
-
-import fr.uphf.Questease.Model.Lobby;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LobbyRepository extends CrudRepository<Lobby, Long> {
-}
